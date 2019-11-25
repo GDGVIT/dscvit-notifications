5fe33a44c20045f7881f0564cc441717990e3d68
Samarth Nayyar <samarthnayyar123@gmail.com>
2019-11-25 20:41:08 +0530

    Added Notification Spam Check

--- js/portal.js
+++ js/portal.js
@@ -2,15 +2,26 @@ let notificationTitle = document.getElementById('title-text-field');
 let notificationBody = document.getElementById('body-text-field');
 let notificationUrl = document.getElementById('url-text-field');
 let sendNotificationButton = document.getElementById('send-notification');
+let allowNotification = true;
 
 if(sessionStorage.getItem('DSCNotifAdminLogin') != 'true'){
     window.location = "/index.html";
 }
 
+function timeOutNotification(){
+    allowNotification = false;
+
+    setTimeout(function(){
+        allowNotification = true;
+    },30000)
+}
+
+
 sendNotificationButton.addEventListener('click', function(){
     
-    
-    fetch('https://dscrec19.herokuapp.com/notifs/send', {
+    if(allowNotification === true){
+
+        fetch('https://dscrec19.herokuapp.com/notifs/send', {
         method: 'POST',
         crossDomain: true,
         headers: {
@@ -33,6 +44,14 @@ sendNotificationButton.addEventListener('click', function(){
         .then(function (responseJSON) {
             console.log(responseJSON);
         })
+
+        timeOutNotification();
+
+    }
+
+    else{
+        alert('You are on 30 second timeout! Stop spamming.')
+    }
 
 
 })
